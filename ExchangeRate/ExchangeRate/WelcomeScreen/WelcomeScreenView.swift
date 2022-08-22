b80e778fdfc47f5d1482cf495f932a7ec735f73f
Anastasia <dianastasiavi@gmail.com>
2022-08-22 16:02:31 +0300

    label

--- ExchangeRate/ExchangeRate/WelcomeScreen/WelcomeScreenView.swift
+++ ExchangeRate/ExchangeRate/WelcomeScreen/WelcomeScreenView.swift
@@ -14,33 +14,57 @@ class WelcomeScreenView: UIView {
     //MARK: Properties
     
     @IBOutlet weak var imageView: UIImageView?
+    @IBOutlet weak var welcomeLabel: UILabel?
     
     // MARK: -
     // MARK: Public Methods
     
-    public func commonSetup(){
+    public func commonSetup() {
         setImageView()
         setImageViewConstraints()
+        setLabel()
+        setLabelConstraints()
     }
     
     // MARK: -
     // MARK: Private Methods
     
-    private func setImageView(){
+    private func setImageView() {
         let exchangeImage = UIImage(named: "1")
         imageView?.image = exchangeImage
-        //imageView?.contentMode = .top
         imageView?.semanticContentAttribute = .forceLeftToRight
         imageView?.contentMode = .scaleAspectFill
-        
     }
     
-    private func setImageViewConstraints(){
+    private func setImageViewConstraints() {
         let safeArea = self.safeAreaLayoutGuide
         imageView?.translatesAutoresizingMaskIntoConstraints = false
         imageView?.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
         imageView?.widthAnchor.constraint(equalTo: safeArea.widthAnchor).isActive = true
-        imageView?.heightAnchor.constraint(equalTo: safeArea.heightAnchor, multiplier: 0.55).isActive = true
+        imageView?.heightAnchor.constraint(equalTo: safeArea.heightAnchor, multiplier: 0.5).isActive = true
+    }
+    
+    private func setLabel() {
+        welcomeLabel?.text = """
+  Хммм...
+  Який курс був тиждень тому? Хто б знав...
+  Ми знаємо! Оберіть дату, що вас цікавить, і курс валют з'явиться на екрані.
+  Удачі!
+  """
+        welcomeLabel?.textAlignment = .center
+        welcomeLabel?.numberOfLines = 5
+        welcomeLabel?.contentMode = .center
+        welcomeLabel?.contentMode = .scaleAspectFill
+    }
+    
+    private func setLabelConstraints() {
+        let safeArea = self.safeAreaLayoutGuide
+        welcomeLabel?.translatesAutoresizingMaskIntoConstraints = false
+        guard let imageBottom = imageView?.bottomAnchor else { return }
+        welcomeLabel?.topAnchor.constraint(equalTo: imageBottom, constant: 10).isActive = true
+        welcomeLabel?.widthAnchor.constraint(equalTo: safeArea.widthAnchor).isActive = true
+        guard let imageHeight = imageView?.heightAnchor else { return }
+        welcomeLabel?.heightAnchor.constraint(equalTo: imageHeight, multiplier: 0.3).isActive = true
     }
 }
 
